b13db797bb633dcc477faeff4093cb4a2064d015
Hector <hectorcreatives08@gmail.com>
2023-07-26 17:34:54 -0400

    add okupados

--- src/index.js
+++ src/index.js
@@ -1,107 +1,3 @@
-// const express = require('express');
-// const xlsx = require('xlsx');
-// const ejs = require('ejs');
-// const app = express();
-// const bodyParser = require('body-parser');
-// const firebase = require('firebase/app');
-// require('firebase/auth');
-// // const firebase = require('./firebaseConfig');
-// const { auth } = require('./firebaseConfig');
-
-
-
-
-// // Configura body-parser para analizar datos en formato JSON
-// app.use(bodyParser.json());
-
-// // Configura body-parser para analizar datos en formato x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// const workbook = xlsx.readFile('data_97.xlsx');
-// const worksheet = workbook.Sheets['Sheet1'];
-// const data = xlsx.utils.sheet_to_json(worksheet);
-
-
-
-
-// app.get('/login', (req, res) => {
-//   res.sendFile(__dirname + '/login.html');
-// });
-
-// app.get('/register', (req, res) => {
-//   res.sendFile(__dirname + '/register.html');
-// });
-
-
-
-// app.post('/register', (req, res) => {
-//   const { email, password } = req.body;
-//   auth.createUserWithEmailAndPassword(email, password)
-//     .then((userCredential) => {
-//       res.status(200).send({ message: 'Usuario registrado con éxito', user: userCredential.user });
-//     })
-//     .catch((error) => {
-//       console.error("Error al registrar usuario:", error.code, error.message);
-//       res.status(400).send({ message: 'Error al registrar el usuario', error: error.message });
-//     });
-// });
-
-// app.post('/login', (req, res) => {
-//   const { email, password } = req.body;
-//   auth.signInWithEmailAndPassword(email, password)
-//     .then((userCredential) => {
-//       res.status(200).send({ message: 'Inicio de sesión exitoso', user: userCredential.user });
-//     })
-//     .catch((error) => {
-//       res.status(400).send({ message: 'Error al iniciar sesión', error: error.message });
-//     });
-// });
-
-
-// app.use((err, req, res, next) => {
-//   console.error(err);
-//   res.status(500).send({ message: 'Error interno del servidor', error: err.message });
-// });
-
-
-
-
-// app.get('/', (req, res) => {
-//   ejs.renderFile('propiedades.html', { data: data }, (err, html) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send('Error al renderizar la página');
-//     } else {
-//       console.log(data);
-//       res.send(html);
-//     }
-//   });
-// });
-
-// app.get('/propiedad/:id', (req, res) => {
-//   const id = req.params.id;
-//   const propiedad = data.find((item) => item.Id == id); // Usa 'Id' y '==' en lugar de 'id' y '==='
-//   console.log(propiedad);
-  
-//   ejs.renderFile('detallePropiedad.html', { propiedad: propiedad }, (err, html) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send('Error al renderizar la página');
-//     } else {
-//       console.log(propiedad)
-//       console.log(id);
-//       res.send(html);
-//     }
-//   });
-// });
-
-
-
-// app.listen(3000, () => {
-//   console.log('Servidor iniciado en el puerto 3000');
-// });
-
-
 const express = require('express');
 const xlsx = require('xlsx');
 const ejs = require('ejs');
@@ -136,7 +32,6 @@ app.use(myconnection(mysql, {
 }));
 
 
-
 app.use(session({
   secret : 'secret',
   resave : true,
@@ -165,131 +60,12 @@ const worksheet6 = workbook6.Sheets['Sheet1'];
 const data6 = xlsx.utils.sheet_to_json(worksheet6);
 const data = data1.concat(data2, data3, data4, data5, data6);
 
+const OcupadosBook = xlsx.readFile(path.resolve(__dirname, 'okupados2.xlsx'));
+const OcupadosSheet = OcupadosBook.Sheets['Sheet1'];
+const dataOcupados = xlsx.utils.sheet_to_json(OcupadosSheet);
 
 
-// app.post('/filtrar', (req, res) => {
 
-
-//   function capitalizeFirstLetter(str) {
-//     if (!str) {
-//       return '';
-//     }
-//     return str[0].toUpperCase() + str.slice(1).toLowerCase();
-//   }
-
-//   req.session.filtro = {
-//     provincia: req.body.provincia,
-//     referencia: req.body.referencia ? req.body.referencia.toUpperCase() : '',
-//     ciudad: req.body.ciudad,
-//     precioMinimo: req.body.precioMinimo,
-//     precioMaximo: req.body.precioMaximo,
-//     busqueda: req.body.busquedas
-//   };
-  
-//   const provincia = req.body.provincia;
-//   const referencia = req.body.referencia ? req.body.referencia.toUpperCase() : '';
-//   const ciudad = req.body.ciudad;
-//   const precioMinimo = req.body.precioMinimo;
-//   const precioMaximo = req.body.precioMaximo;
-//   const busqueda = req.body.busqueda; 
-
-  
-//   const propiedadesTotales = data.length;
-//   let propiedadesFiltradas = 0;
-
-
-//   const filteredData = data.filter(item => {
-//     if (!item.Price) {
-//       return false;
-//     }
-//     const itemPrice = parseFloat(item.Price.replace('€', '').replace('.', '').replace('.','').replace('.','').trim());
-//     const isProvinciaMatch = !provincia || (item.Provincia && item.Provincia.toLowerCase().includes(provincia.toLowerCase()));
-//     const isCiudadMatch = !ciudad || (item.Municipio && item.Municipio.toLowerCase().includes(ciudad.toLowerCase()));
-//     // const isReferenciaMatch = !referencia || (item.Id && item.Id.toLowerCase().includes(referencia.toLowerCase()));
-//     const isReferenciaMatch = !referencia || (item.Id && item.Id.toString().toLowerCase().includes(referencia.toLowerCase()));
-
-//     const isBusquedaMatch = !busqueda || (
-//       (item.Provincia && item.Provincia.toLowerCase().includes(busqueda.toLowerCase())) ||
-//       (item.Municipio && item.Municipio.toLowerCase().includes(busqueda.toLowerCase())) ||
-//       (item.Id && item.Id.toString().toLowerCase().includes(busqueda.toLowerCase())) ||
-//       (item.Title && item.Title.toLowerCase().includes(busqueda.toLowerCase())) ||
-//       (item.Direccion && item.Direccion.toLowerCase().includes(busqueda.toLowerCase()))
-//     );
-//     const isPrecioMinimoMatch = !precioMinimo || itemPrice >= precioMinimo;
-//     const isPrecioMaximoMatch = !precioMaximo || itemPrice <= precioMaximo;  
-
-//     if (isProvinciaMatch || isCiudadMatch || isReferenciaMatch || isBusquedaMatch || isPrecioMinimoMatch || isPrecioMaximoMatch) {
-//       propiedadesFiltradas++;
-//     }
-
-//     return isProvinciaMatch && isCiudadMatch && isReferenciaMatch && isBusquedaMatch && isPrecioMinimoMatch && isPrecioMaximoMatch;
-//   });
-   
-//   const page = parseInt(req.query.page) || 1;
-//   const limit = 200;
-//   const skip = (page - 1) * limit;
-  
-//   const paginatedData = filteredData.slice(skip, skip + limit);
-//   const totalPropiedadesFiltradas = filteredData.length;
-//   const totalPropiedades = totalPropiedadesFiltradas;
-//   const totalPages = Math.ceil(totalPropiedades / limit);
-//   const pages = [];
-//   for (let i = 1; i <= totalPages; i++) {
-//     pages.push(i);
-//   }
-
-//   const propiedadesNoFiltradas = propiedadesTotales - propiedadesFiltradas;
-
-//   const propiedadesFiltradasBusqueda = filteredData.reduce((count, item) => {
-//     const itemPrice = parseFloat(item.Price.replace('€', '').replace('.', '').replace('.','').replace('.','').trim());
-//     const isProvinciaMatch = !provincia || (item.Provincia && item.Provincia.toLowerCase().includes(provincia.toLowerCase()));
-//     const isCiudadMatch = !ciudad || (item.Municipio && item.Municipio.toLowerCase().includes(ciudad.toLowerCase()));
-//     // const isReferenciaMatch = !referencia || (item.Id && item.Id.toLowerCase().includes(referencia.toLowerCase()));
-//     const isReferenciaMatch = !referencia || (item.Id && item.Id.toString().toLowerCase().includes(referencia.toLowerCase()));
-
-//     const isBusquedaMatch = !busqueda || (
-//       (item.Provincia && item.Provincia.toLowerCase().includes(busqueda.toLowerCase())) ||
-//       (item.Municipio && item.Municipio.toLowerCase().includes(busqueda.toLowerCase())) ||
-//       (item.Id && item.Id.toString().toLowerCase().includes(busqueda.toLowerCase())) ||
-//       (item.Title && item.Title.toLowerCase().includes(busqueda.toLowerCase())) ||
-//       (item.Direccion && item.Direccion.toLowerCase().includes(busqueda.toLowerCase()))
-//     );
-//     const isPrecioMinimoMatch = !precioMinimo || itemPrice >= precioMinimo;
-//     const isPrecioMaximoMatch = !precioMaximo || itemPrice <= precioMaximo;  
-    
-//     if (isProvinciaMatch || isCiudadMatch || isReferenciaMatch || isBusquedaMatch || isPrecioMinimoMatch || isPrecioMaximoMatch) {
-//       return count + 1;
-//     }
-//     return count;
-//   }, 0);
-
-
-//   res.render('home', { data: paginatedData, 
-//     pages, 
-//     name: req.session.name, 
-//     totalPropiedadesFiltradas, 
-//     provincia, 
-//     propiedadesTotales, 
-//     referencia, 
-//     ciudad, 
-//     precioMaximo, 
-//     precioMinimo, 
-//     propiedadesNoFiltradas, 
-//     propiedadesFiltradasBusqueda,
-//     busqueda,
-//     precioMinimo: req.session.filtro.precioMinimo,
-//     precioMaximo: req.session.filtro.precioMaximo,
-//     }, (err, html) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send('Error al renderizar la página');
-//     } else {
-//       console.log('Valores enviados al renderizado:', totalPropiedadesFiltradas, propiedadesFiltradasBusqueda);
-
-//       res.send(html);
-//     }
-//   });
-// });
 
 
 app.post('/filtrar', (req, res) => {
@@ -314,7 +90,6 @@ app.post('/filtrar', (req, res) => {
   const precioMinimo = req.body.precioMinimo;
   const precioMaximo = req.body.precioMaximo;
   const busqueda = req.body.busqueda;
-
   const propiedadesTotales = data.length;
   let propiedadesFiltradas = 0;
 
@@ -418,24 +193,30 @@ app.post('/filtrar', (req, res) => {
 });
 
 
+app.get('/okupados', (req, res) => {
+  const data = dataOcupados.map((item) => {
+    return {
+      referencia: item.REFERENCIA,
+      direccion: item.DIRECCION,
+      estatus: item.ESTATUS,
+      ocupado: item.OCUPADO,
+      tipo: item.TIPO,
+      fondo: item.FONDO,
+      precio: item.PRECIO
+    };
+  });
+
+  console.log(data); // Agregado para imprimir los datos de dataOcupados
+
+  res.render('okupados', { 
+    data: data,
+    name: req.session.name 
+  });
+});
 
 app.get('/detalle/:id', (req, res) => {
   const id = req.params.id;
   const detalle = data.find((item) => item.Id == id); // Usa 'Id' y '==' en lugar de 'id' y '==='
-
-  // if (detalle && detalle.ImageSources) {
-  //   // Verificar si ImageSources es una cadena
-  //   if (typeof detalle.ImageSources === 'string') {
-  //     // Reemplazar las comillas simples por comillas dobles
-  //     detalle.ImageSources = detalle.ImageSources.replace(/'/g, '"');
-  
-  //     // Convertir la cadena "ImageSources" en un arreglo
-  //     detalle.ImageSources = JSON.parse(detalle.ImageSources);
-  //   } else {
-  //     // Si ImageSources no es una cadena, asignar un arreglo vacío
-  //     detalle.ImageSources = [];
-  //   }
-  // }
 
   if (typeof detalle.ImageSources === 'string') {
     try {
@@ -447,8 +228,6 @@ app.get('/detalle/:id', (req, res) => {
   } else {
     detalle.ImageSources = [];
   }
-  
-
   console.log(detalle);
   
   res.render('detallePropiedad', { detalle: detalle, name: req.session.name }, (err, html) => {
@@ -465,7 +244,6 @@ app.get('/detalle/:id', (req, res) => {
 
 
 app.use('/', loginRoutes);
-
 
 const hbs = require('handlebars');
 
@@ -490,7 +268,6 @@ hbs.registerHelper('if_or', function() {
     return opts.inverse(this);
   }
 });
-
 
 // Registrar un helper personalizado en Handlebars
 hbs.registerHelper('checkMainPhoto', function(MainPhoto, options) {
@@ -521,23 +298,42 @@ app.get('/', (req, res) => {
 
     // Usar los criterios de búsqueda de la sesión para filtrar
     const filtro = req.session.filtro || {};
-    const filteredData = data.filter(item => {
-      // Aplicar el filtro basado en los criterios de búsqueda
-        const itemPrice = item.Price ? parseFloat(item.Price.replace('€', '').replace('.', '').replace('.','').replace('.','').trim()) : 0;
-        const isProvinciaMatch = !filtro.provincia || (item.Provincia && item.Provincia.toLowerCase().includes(filtro.provincia.toLowerCase()));
-        const isCiudadMatch = !filtro.ciudad || (item.Municipio && item.Municipio.toLowerCase().includes(filtro.ciudad.toLowerCase()));
-        const isReferenciaMatch = !filtro.referencia || (item.Id && item.Id.toLowerCase().includes(filtro.referencia.toLowerCase()));
-        const isBusquedaMatch = !filtro.busqueda || (
-          (item.Provincia && item.Provincia.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
-          (item.Municipio && item.Municipio.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
-          (item.Id && item.Id.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
-          (item.Title && item.Title.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
-          (item.Direccion && item.Direccion.toLowerCase().includes(filtro.busqueda.toLowerCase()))
-        );
-        const isPrecioMinimoMatch = !filtro.precioMinimo || itemPrice >= filtro.precioMinimo;
-        const isPrecioMaximoMatch = !filtro.precioMaximo || itemPrice <= filtro.precioMaximo;  
+    // const filteredData = data.filter(item => {
+    //   // Aplicar el filtro basado en los criterios de búsqueda
+    //     const itemPrice = item.Price ? parseFloat(item.Price.replace('€', '').replace('.', '').replace('.','').replace('.','').trim()) : 0;
+    //     const isProvinciaMatch = !filtro.provincia || (item.Provincia && item.Provincia.toLowerCase().includes(filtro.provincia.toLowerCase()));
+    //     const isCiudadMatch = !filtro.ciudad || (item.Municipio && item.Municipio.toLowerCase().includes(filtro.ciudad.toLowerCase()));
+    //     const isReferenciaMatch = !filtro.referencia || (item.Id && item.Id.toLowerCase().includes(filtro.referencia.toLowerCase()));
+    //     const isBusquedaMatch = !filtro.busqueda || (
+    //       (item.Provincia && item.Provincia.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
+    //       (item.Municipio && item.Municipio.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
+    //       (item.Id && item.Id.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
+    //       (item.Title && item.Title.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
+    //       (item.Direccion && item.Direccion.toLowerCase().includes(filtro.busqueda.toLowerCase()))
+    //     );
+    //     const isPrecioMinimoMatch = !filtro.precioMinimo || itemPrice >= filtro.precioMinimo;
+    //     const isPrecioMaximoMatch = !filtro.precioMaximo || itemPrice <= filtro.precioMaximo;  
 
-        return isProvinciaMatch && isCiudadMatch && isReferenciaMatch && isBusquedaMatch && isPrecioMinimoMatch && isPrecioMaximoMatch;
+    //     return isProvinciaMatch && isCiudadMatch && isReferenciaMatch && isBusquedaMatch && isPrecioMinimoMatch && isPrecioMaximoMatch;
+    // });
+
+    const filteredData = data.filter(item => {
+      const itemPrice = item.Price ? parseFloat(item.Price.replace('€', '').replace('.', '').replace('.','').replace('.','').trim()) : 0;
+      const isProvinciaMatch = !filtro.provincia || (item.Provincia && item.Provincia.toLowerCase().includes(filtro.provincia.toLowerCase()));
+      const isCiudadMatch = !filtro.ciudad || (item.Municipio && item.Municipio.toLowerCase().includes(filtro.ciudad.toLowerCase()));
+      const idValue = typeof item.Id === 'string' ? item.Id : '';
+      const isReferenciaMatch = !filtro.referencia || (idValue && idValue.toLowerCase().includes(filtro.referencia.toLowerCase()));
+      const isBusquedaMatch = !filtro.busqueda || (
+        (item.Provincia && item.Provincia.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
+        (item.Municipio && item.Municipio.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
+        (idValue && idValue.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
+        (item.Title && item.Title.toLowerCase().includes(filtro.busqueda.toLowerCase())) ||
+        (item.Direccion && item.Direccion.toLowerCase().includes(filtro.busqueda.toLowerCase()))
+      );
+      const isPrecioMinimoMatch = !filtro.precioMinimo || itemPrice >= filtro.precioMinimo;
+      const isPrecioMaximoMatch = !filtro.precioMaximo || itemPrice <= filtro.precioMaximo;  
+    
+      return isProvinciaMatch && isCiudadMatch && isReferenciaMatch && isBusquedaMatch && isPrecioMinimoMatch && isPrecioMaximoMatch;
     });
 
     const totalPropiedades = filteredData.length;
@@ -548,6 +344,7 @@ app.get('/', (req, res) => {
       pages.push(i);
     }
 
+  
     const paginatedData = filteredData.slice(skip, skip + limit);
     res.render('home', { data: paginatedData, pages, name: req.session.name, totalPropiedades }, (err, html) => {
       if (err) {
@@ -562,42 +359,6 @@ app.get('/', (req, res) => {
       res.redirect('/login');
   }   
 });
-
-
-
-
-
-
-// app.get('/', (req, res) => {
-//   if(req.session.loggedin == true) {
-//     const page = parseInt(req.query.page) || 1;
-//   const limit = 200;
-//   const skip = (page - 1) * limit;
-
-//   const totalPropiedades = data.length;
-//   const totalPages = Math.ceil(totalPropiedades / limit);
-
-//   const pages = [];
-//   for (let i = 1; i <= totalPages; i++) {
-//     pages.push(i);
-//   }
-
-//   const paginatedData = data.slice(skip, skip + limit);
-//   res.render('home', { data: paginatedData, pages, name: req.session.name, totalPropiedades }, (err, html) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send('Error al renderizar la página');
-//     } else {
-//       console.log(paginatedData);
-//       res.send(html);
-//     }
-//   });
-//   }else{
-//       res.redirect('/login');
-//   }   
-
-  
-// });
 
 
 app.listen(3000, () => {
